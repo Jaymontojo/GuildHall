f3d80b106b3faa886a4ba44ae6a273544e239073
Code Chrysalis <codechrysalis@Codes-Mac-mini.local>
2020-11-02 23:01:37 +0900

    added package react-router-dom. Will work on the router tomorrow

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,17 +1,41 @@
-import React, { useRef } from 'react';
-import { Form, Button, Card } from 'react-bootstrap';
+import React, { useRef, useState } from 'react';
+import { Form, Button, Card, Alert } from 'react-bootstrap';
+import { useAuth } from '../contexts/AuthContext'
 
 export default function Signup() {
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
+  const { signup } = useAuth();
+  const [error, setError] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    //validation checks
+    if (passwordRef.current.value === passwordConfirmRef.current.value) {
+      return setError('Passwords do not match!')
+    }
+    try {
+      setError('')
+      //prevents submit button from allowing a user to generate multiple users at one instance
+      setLoading(true)
+      await signup(emailRef.current.value, passwordRef.current.value)
+    } catch {
+      setError('Registration attempt failed! Account was not created.')
+    }
+    //returns the submit buttons state to normal
+    setLoading(false)
+  }
 
   return (
     <div>
       <Card>
         <Card.Body>
           <h2 className= "text-center mb-4">Sign Up</h2>
-          <Form>
+          {error && <Alert variant = "danger"> {error}</Alert>}
+          <Form onSubmit = { handleSubmit }>
             <Form.Group id="email">
               <Form.Label>Email</Form.Label>
               <Form.Control type="email" ref = { emailRef }required />
@@ -24,7 +48,7 @@ export default function Signup() {
               <Form.Label>Password Confirmation</Form.Label>
               <Form.Control type="password-confirm" ref = { passwordConfirmRef }required />
             </Form.Group>
-            <Button className="w-100" type="submit">Sign Up</Button>
+            <Button disabled = { loading } className="w-100" type="submit">Sign Up</Button>
           </Form>
         </Card.Body>
       </Card>
